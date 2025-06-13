66ed5a017945ca67b234b68da75a422a569f446f
Sv443 <contact@sv443.net>
2025-06-14 00:18:52 +0200

    ref: rename index to main

--- src/cli.mjs
+++ src/cli.mjs
@@ -2,15 +2,18 @@
 
 import { spawn } from "node:child_process";
 import { join } from "node:path";
+import { scheduleExit } from "@sv443-network/coreutils";
 
 const args = [
   "--no-warnings=ExperimentalWarning",
   "--enable-source-maps",
   "--loader=ts-node/esm",
-  join(import.meta.dirname, "./index.ts"),
+  join(import.meta.dirname, "./main.ts"),
   ...process.argv.slice(2),
-  "--caller-path",
-  `"${btoa(encodeURIComponent(process.cwd()))}"`,
+  ...(process.argv.every(a => !a.includes("caller-path")) ? [
+    "--caller-path",
+    `"${btoa(encodeURIComponent(process.cwd()))}"`,
+  ] : []),
 ];
 
 const child = spawn("node", args, {
@@ -18,11 +21,9 @@ const child = spawn("node", args, {
   cwd: join(import.meta.dirname, "../"),
 });
 
-child.on("exit", (code, signal) =>
-  setImmediate(() => {
-    if(code)
-      process.exit(code);
-    else if(signal)
-      process.kill(process.pid, signal);
-  })
-);
+child.on("exit", (code, signal) => {
+  if(code)
+    scheduleExit(code);
+  else if(signal)
+    process.kill(process.pid, signal);
+});
